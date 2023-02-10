f8d645eda346b97ba8127f646baa87cc8dd71a17
kendumboiz <leducnghi28122000@gmail.com>
2023-02-10 22:42:00 +0700

    update code

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,9 @@
-import About from "../../components/Layouts/Home/About/About";
-import Banner from "../../components/Layouts/Home/HomeBanner/HomeBanner";
-import BestSeller from "../../components/Layouts/Home/BestSeller/BestSeller";
-import Collection from "../../components/Layouts/Home/Collection/Collection";
-import Connect from "../../components/Layouts/Home/Connect/Connect";
-import Feedback from "../../components/Layouts/Home/Feedback/Feedback";
+import About from "./About/About";
+import Banner from "./HomeBanner/HomeBanner";
+import BestSeller from "./BestSeller/BestSeller";
+import Collection from "./Collection/Collection";
+import Connect from "./Connect/Connect";
+import Feedback from "./Feedback/Feedback";
 import ScrollToTop from "components/Common/ScrollToTop/ScrollToTop";
 import UsersLayout from "../../components/Layouts/Users/UsersLayout";
 
