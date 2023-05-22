f2f8b71c37055840eca82b51d21a3eabec040d6f
LeDucNghi <81682075+LeDucNghi@users.noreply.github.com>
2023-05-22 21:40:38 +0700

    Update code

--- src/app/store.js
+++ src/app/store.js
@@ -3,13 +3,10 @@ import rootReducer from "./rootReducer";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import thunk from "redux-thunk";
 
-// import { productApi } from "services/productService";
-
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
-      // productApi.middleware,
       thunk
     ),
 });
